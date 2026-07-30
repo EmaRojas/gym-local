078d6c4088f3ff64d0fba93218b965e610a95af9
Emanuel Rojas <emarojas710@gmail.com>
2026-07-30 18:21:03 -0300

    orden de carga

--- src/views/ExercisesBrowserView.vue
+++ src/views/ExercisesBrowserView.vue
@@ -146,7 +146,7 @@
 </template>
 
 <script lang="ts">
-import { ref, computed, watch, onMounted, onUnmounted } from 'vue'
+import { ref, computed, onMounted, onUnmounted } from 'vue'
 import { useExercises, translateCategory, translateEquipment, translateName } from '../composables/useExercises'
 import type { Exercise } from '../types'
 
@@ -178,16 +178,8 @@ export default {
     } = useExercises()
 
     onMounted(() => {
-      if (mostrarResultados.value) loadExercises(props.adminId || undefined)
+      loadExercises(props.adminId || undefined)
     })
-    watch(
-      [searchQuery, categoryFilter, equipmentFilter],
-      () => {
-        if (mostrarResultados.value && !exercisesLoaded.value) {
-          loadExercises(props.adminId || undefined)
-        }
-      }
-    )
 
     const mostrarResultados = computed(() =>
       searchQuery.value.length >= 4 || !!categoryFilter.value || !!equipmentFilter.value
